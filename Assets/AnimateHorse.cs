5981c29e29197c6ac8d39bc2830a6134d1357960
Giedrius <266934@via.dk>
2020-09-23 21:04:21 +0200

    Added virtual button to CLubs horse, now plays animation of galloping when it is activated, idle when released

--- Assets/AnimateHorse.cs
+++ Assets/AnimateHorse.cs
@@ -1,51 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class AnimateHorse : MonoBehaviour
+public class AnimateHorse : MonoBehaviour, IVirtualButtonEventHandler
 {
-    public float pos;
-    public float newPos;
-    public double posRounded;
-    public double newPosRounded;
-
     public GameObject horseObj;
     public Animator animController;
     public string animControllerName;
 
-    public bool playAnim = false;
+    public GameObject vbBtnObj;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        animController = horseObj.gameObject.GetComponent<Animator>();
-        animController.runtimeAnimatorController = Resources.Load($"{animControllerName}") as RuntimeAnimatorController;
+        vbBtnObj = GameObject.Find("MoveClubsHorseBtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
 
-        pos = transform.position.z;
-        posRounded = System.Math.Round(pos,3);
+        animController = horseObj.gameObject.GetComponent<Animator>();
+        animController.runtimeAnimatorController = Resources.Load($"{animControllerName}") as RuntimeAnimatorController;        
     }
 
     // Update is called once per frame
     void Update()
     {
-        newPos = transform.position.z;
-        newPosRounded = System.Math.Round(newPos,3);
+    }
 
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
         if (animController != null && animController.runtimeAnimatorController != null)
-            {
-                animController.SetBool("isRunning", false);               
-            }
-
-        if(newPosRounded != posRounded)
         {
-            Debug.Log("Moving! Old position Z: " + posRounded + ", New position Z: " + newPosRounded);
+            animController.SetBool("isRunning", true);    
+            Debug.Log("Button pressed!");       
+        }
+    }
 
-            posRounded = newPosRounded;
-
-            if (animController != null && animController.runtimeAnimatorController != null)
-            {
-                animController.SetBool("isRunning", true);               
-            }
-        }   
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        if (animController != null && animController.runtimeAnimatorController != null)
+        {
+            animController.SetBool("isRunning", false);       
+            Debug.Log("Button released!");       
+        }
     }
 }
